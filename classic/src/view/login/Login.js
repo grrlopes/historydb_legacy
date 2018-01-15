e26afea622f9d4478684aeb4550f9b89c25c06b7
Gabriel Lopes <gabrielrrlopes@gmail.com>
2018-01-15 02:40:52 -0200

    Adicionado cadastro de user no login

--- classic/src/view/login/Login.js
+++ classic/src/view/login/Login.js
@@ -9,35 +9,88 @@ Ext.define('hdb.view.login.Login', {
 
     controller: 'loginviewctrl',
 
-    bodyPadding: 10,
+    bodyPadding: 1,
     resizable: false,
     title: 'Credencial',
     closable: false,
     autoShow: true,
+    border: false,
 
-    items: {
-        xtype: 'form',
+    items: [{
+        region: 'center',
+        xtype: 'tabpanel',
+        activeTab: 0,
+        split: false,
+        border: false,
+        defaults: {
+            layout: 'fit'
+        },
         items: [{
-            xtype: 'textfield',
-            name: 'user',
-            fieldLabel: 'Usuário',
-            allowBlank: false
-        }, {
-            xtype: 'textfield',
-            name: 'pass',
-            inputType: 'password',
-            fieldLabel: 'Senha',
-            allowBlank: false
-        }, {
-            xtype: 'displayfield',
-            hideEmptyLabel: false
-        }],
-        buttons: [{
-            text: 'Logar',
-            formBind: true,
-            listeners: {
-                click: 'onLogin'
-            }
+            title: 'Login',
+            items: [{
+                width: 375,
+                height: 190,
+                xtype: 'form',
+                defaults: {
+                    labelAlign: 'top'
+                },
+                items: [{
+                    xtype: 'textfield',
+                    name: 'user',
+                    fieldLabel: 'Usuário',
+                    allowBlank: false
+                },{
+                    xtype: 'textfield',
+                    name: 'pass',
+                    inputType: 'password',
+                    fieldLabel: 'senha',
+                    allowBlank: false
+                },{
+                    xtype: 'displayfield',
+                    hideEmptyLabel: false
+                }],
+                buttons: [{
+                    text: 'Logar',
+                    formBind: true,
+                    listeners: {
+                        click: 'onLogin'
+                    }
+                }]
+            }]
+        },{
+            title: 'Cadastro',
+            items: [{
+                width: 375,
+                height: 250,
+                xtype: 'form',
+                defaults: {
+                    labelAlign: 'top'
+                },
+                items: [{
+                    xtype: 'textfield',
+                    name: 'user',
+                    fieldLabel: 'Usuário',
+                    allowBlank: false
+                },{
+                    xtype: 'textfield',
+                    name: 'pass',
+                    inputType: 'password',
+                    fieldLabel: 'senha',
+                    allowBlank: false
+                },{
+                    xtype: 'textfield',
+                    name: 'cpass',
+                    fieldLabel: 'Confirme senha',
+                    allowBlank: false
+                }],
+                buttons: [{
+                    text: 'Cadastro',
+                    formBind: true,
+                    listeners: {
+                        click: 'onCadastro'
+                    }
+                }]
+            }]
         }]
-    }
+    }]
 });
