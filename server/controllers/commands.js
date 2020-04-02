d6edb1a272117ed256592214427bc42804918d9f
Gabriel Lopes <gabrielrrlopes@gmail.com>
2020-04-02 04:49:54 -0400

    Added method http get for retriving multiple/single value from database

--- server/controllers/commands.js
+++ server/controllers/commands.js
@@ -3,16 +3,36 @@ const Commands = require('../models/commands');
 exports.getCommands = (req, res, next) => {
 	Commands.find()
 		.then(result => {
+			if (result === null || result.length == 0) {
+				const error = new Error('There are not records.');
+				error.statusCode = 404;
+				throw error;
+			};
 			res.json({ data: result });
 		})
-		.catch()
+		.catch(error => {
+			if (!error.statusCode) {
+				error.statusCode = 500;
+			}
+			res.json({ message: err.message, code: err.statusCode })
+		});
 };
 
 exports.getCommand = (req, res, next) => {
 	id = req.body.id;
-	Commands.findById({_id: id})
+	Commands.findById({ _id: id })
 		.then(result => {
-			res.json({data: result})
+			if (result === null || result.length == 0) {
+				const error = new Error('There is no record.');
+				error.statusCode = 404;
+				throw error;
+			};
+			res.json({ data: result });
 		})
-		.catch()
+		.catch(err => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+			res.json({ message: err.message, code: err.statusCode })
+		});
 }
